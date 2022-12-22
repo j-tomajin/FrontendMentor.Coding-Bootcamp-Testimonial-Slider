0ff57ab12b14e540cb451e8992d935bc81195b01
Jerickson Tomajin <tomajinj@gmail.com>
2022-12-22 21:28:35 +0800

    added comment

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 const group = document.getElementsByClassName('group')
 
+// get the current data index
 let activeIndex = 0
 
 const prevSlider = () => {
+    // BUMP index
     const active = activeIndex - 1 >= 0 ? activeIndex - 1 : group.length - 1
 
     const activeGroup = document.querySelector(`[data-index="${activeIndex}"]`),
@@ -17,11 +19,14 @@ const prevSlider = () => {
     setTimeout(() => {
         // set the after to active for smooth transition
         beforeGroup.dataset.status = 'active'
+
+        // updata the current index/ active index 
         activeIndex = active
     })
 }
 
 const nextSlider = () => {
+    // BUMP index
     const active = activeIndex + 1 <= group.length - 1 ? activeIndex + 1 : 0
 
     const activeGroup = document.querySelector(`[data-index="${activeIndex}"]`),
@@ -36,6 +41,8 @@ const nextSlider = () => {
     setTimeout(() => {
         // set the after into active for smooth transition
         nextGroup.dataset.status = 'active'
+
+        // updata the current index/ active index 
         activeIndex = active
     })
 }
